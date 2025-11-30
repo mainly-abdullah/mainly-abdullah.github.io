5bdeb07d6d1d41e9159f2aba7c20cad33c1fc95a
mainly-abdullah <m.abdullah128ull18@gmail.com>
2025-12-01 00:30:05 +0500

    Update

--- Assignment/script.js
+++ Assignment/script.js
@@ -1,14 +1,15 @@
 var names=new Array();
-names[0]="Eris";
-names[1]="Roxy";
-names[2]="Elinalise";
-names[3]="Lilia";
-names[4]="Paul";
-names[5]="Rudius";
-names[6]="Ren";
-names[7]="aisha";
-names[8]="Zenith";
-names[9]="Sylphie";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
+
 
 for (var i = 0; i < names.length; i++) {
 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
